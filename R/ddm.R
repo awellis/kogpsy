1af676056371200882a29710109bf50393aee387
Andrew Ellis <andrewellis@pm.me>
2021-03-25 08:30:57 +0100

    add driftrate to output

--- R/ddm.R
+++ R/ddm.R
@@ -56,6 +56,7 @@ drift_diffusion <- function(bias = 0.5,
     out <- dplyr::tibble(time = round(seq_along(dv) * dt, 2),
                          dv = dv,
                          steps = seq_along(dv),
+                         driftrate = driftrate,
                          decision_boundary = decision_boundary,
                          bias = bias)
     # invisible(dv)
